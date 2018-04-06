04a5bd275d1109fe42ac63ac0b5f411983f94581
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-06 15:04:56 +0700

    Add files via upload

---
+++ display_t.php
@@ -0,0 +1,12 @@
+﻿
+<html>
+<head><title> Classroom</title></head>
+
+<frameset rows="80,50,*" cols="*" frameborder="no" border="0" framespacing="0">
+<frame src="topclass.html" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
+<frame src="top2.html" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
+<frame src="home1.html" name="mainFrame" id="mainFrame" 	title="mainFrame" >
+		
+		
+</frameset>
+</html>
