c2cc38edc91c421ad88ff0590bbaabbb83b5e5fd
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-06 11:12:34 +0700

    Add files via upload

---
+++ home_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<a href="https://webserv.kmitl.ac.th/lampofgod/login_t.php">
+<h1>Teacher</h1>
+</a>
+
+<a href="https://webserv.kmitl.ac.th/lampofgod/login_s.php">
+<h1>Student</h1>
+</a>
+
+</body>
+</html>
---
+++ login_s.php
@@ -0,0 +1,25 @@
+<html>
+
+<body>
+<form name="form1" method="post" action="file:///C|/Users/akaka.LAPTOP-A9P1RJ8E/Desktop/New folder/check_login.php">
+  Student login<br>
+  <table border="1" style="width: 300px">
+    <tbody>
+      <tr>
+        <td> &nbsp;Username</td>
+        <td>
+          <input name="txtUsername" type="text" id="txtUsername">
+        </td>
+      </tr>
+      <tr>
+        <td> &nbsp;Password</td>
+        <td><input name="txtPassword" type="password" id="txtPassword">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <input type="submit" name="Submit" value="Login">
+</form>
+</body>
+</html>
