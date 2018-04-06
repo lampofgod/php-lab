e9065355ecda7bc8f220bff35b2d8590784207c0
nanchonni <38050284+nanchonni@users.noreply.github.com>
2018-04-06 12:48:44 +0700

    Update home_test.php

--- home_test.php
+++ home_test.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <title>Untitled Document</title>
+
 </head>
 
+
 <body>
+<p align="center">
 
-<a href="https://webserv.kmitl.ac.th/lampofgod/login_t.php">
-<h1>Teacher</h1>
+<img src = "whoareyou.png" VSPACE = "100"><br>
+<a href="https://webserv.kmitl.ac.th/lampofgod/login_t.php"><img src = "buttonteacher.png" >
 </a>
 
-<a href="https://webserv.kmitl.ac.th/lampofgod/login_s.php">
-<h1>Student</h1>
+
+<a href="https://webserv.kmitl.ac.th/lampofgod/login_s.php"><img src = "buttonstudent.png" >
 </a>
 
+
+</p>
 </body>
+
 </html>
