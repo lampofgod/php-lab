17c01a49512e1a9ce79c3920ecd37a98588edf5f
sillysoul1998 <38059572+sillysoul1998@users.noreply.github.com>
2018-04-07 16:15:40 +0700

    Update top2.php

--- top2.php
+++ top2.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<style>
+.button {
+    background-color: #0e5285;
+    width: 15%;
+    border: none;
+    color: white;
+    padding: 15px 40px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: -7px 2px;
+    cursor: pointer;
+}//botton color 
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
 
-<body>
-<a href="announcement.php" target="mainFrame">Announcement</a>
-<a href="work.php" target="mainFrame">Work</a>
-<a href="student_frame.php" target="mainFrame">Student</a>
-<a href="file.php" target="mainFrame">File</a>
-<a href="Blog.php" target="mainFrame">Blog</a>
+
+<body bgcolor="#bde3ff"> 
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="announcement.php"> <input  value="Announcement" align="center"    class="button" target="mainFrame"></a>
+<a href="work.php">			<input  value="Work"		 align="center"    class="button" target="mainFrame"></a>
+<a href="student_frame.php"><input  value="Student" 	 align="center"    class="button" target="mainFrame"></a>
+<a href="file.php">			<input  value="File"		 align="center"    class="button" target="mainFrame"></a>
+<a href="Blog.php">			<input  value="Blog" 		 align="center"    class="button" target="mainFrame"></a>
+
 </body>
 </html>
