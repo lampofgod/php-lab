16fbdad311a90988d846d63ba8a7f59f09d6a817
realsoftwarex <38059737+realsoftwarex@users.noreply.github.com>
2018-04-07 13:32:20 +0700

    Update create_class.php

--- create_class.php
+++ create_class.php
@@ -1,17 +1,45 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<style>
+div{
+	width: 800px;
+	height: 250px;
+	border: 3px solid #0e5285;
+
+}//table
+.button {
+    background-color: #0e5285;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}//botton color
+h3{
+	font-style: bold;
+	color: #0e5285;
+}
+
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
 
 <body>
+
 <form name="form1" method="post" action="save_create_class.php">
-<h1>Create class</h1>
-<br />
+
+<center><div><br>
+
+<h3>Create class</h3>
 Class name <br /><input name="Classname" type="text" id="Classname" size="100"><br />
-About class <br /><input name="Aboutclass" type="text" id="Aboutclass" size="100"><br />
-<input type="submit" name="Submit" value="Save"></form>
+About class <br /><input name="Aboutclass" type="text" id="Aboutclass" size="100"><br /><br>
+<input type="submit" name="Submit" value="Save" class="button"></div></center></form>
 
 </body>
 </html>
