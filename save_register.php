be85c21bdcee8d27f02b315c81af30f9d0bca83c
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-07 15:50:16 +0700

    Update save_register.php

--- save_register.php
+++ save_register.php
@@ -1,56 +1,66 @@
+<html>
+<body>
 <?php
 	include("config.php");
 	
 	if(trim($_POST["txtUsername"]) == "")
 	{
-		echo "Please input Username!";
-		exit();	
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Username!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
+		exit();
 	}
 	
 	if(trim($_POST["txtPassword"]) == "")
 	{
-		echo "Please input Password!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Password!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}	
 		
 	if($_POST["txtPassword"] != $_POST["txtConPassword"])
 	{
-		echo "Password not Match!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Password not Match!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';		
 		exit();
 	}
 	
 	if(trim($_POST["txtName"]) == "")
 	{
-		echo "Please input Name!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Name!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}
 	if(trim($_POST["txtLastname"]) == "")
 	{
-		echo "Please input Lastname!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Lastname!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}
 	if(trim($_POST["intAge"]) == "")
 	{
-		echo "Please input Age!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Age!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}
 	if(trim($_POST["txtUniversity"]) == "")
 	{
-		echo "Please input School or University!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input School or University!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}
 	if(trim($_POST["txtEmail"]) == "")
 	{
-		echo "Please input Email!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Email!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 		exit();	
 	}
-
 	$strSQL = "SELECT * FROM member WHERE txtUsername = '".trim($_POST['txtUsername'])."' ";
 	$objQuery = mysqli_query($objCon,$strSQL);
 	$objResult = mysqli_fetch_array($objQuery);
 	if($objResult)
 	{
-			echo "Username already exists!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Username already exists!</font>";
+		echo '<br><br><a href="javascript:history.go(-1)"><img src="back.png"/></a>';
 	}
 	else
 	{
@@ -59,13 +69,13 @@
 		'".$_POST["txtPassword"]."','".$_POST["txtName"]."','".$_POST["txtLastname"]."','".$_POST["ddlStatus"]."','".$_POST["intAge"]."','".$_POST["txtUniversity"]."','".$_POST["txtEmail"]."')";
 		$objQuery = mysqli_query($objCon,$strSQL);
 		
-		echo "Register Completed!<br>";		
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Register Completed!<br></font>";		
 	    if(trim($_POST["Status"]) == "STUDENT"){
-		echo "<br> Go to <a href='login_s.php'>Login page</a>";}
-		else{echo "<br> Go to <a href='login_t.php'>Login page</a>";
+		echo "<br><font face=\"Cambria\" color=\"062144\">Go to <a href='login_s.php'>Login page</a></font>";}
+		else{echo "<br><font face=\"Cambria\" color=\"062144\"> Go to <a href='login_t.php'>Login page</a></font>";
 		}
-		
 	}
-
 	mysqli_close($objCon);
 ?>
+</body>
+</html>
