04696e415b0ea2d2a5274a9e859e813202f5ea1e
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-08 15:10:08 +0700

    Update add_class.php

--- add_class.php
+++ add_class.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
+
 </head>
 
-<body>
-<form name="form1" method="post" action="save_class.php">
-Enter your class code <input name="Classcode" type="text" id="Classcode">
-<input type="submit" name="Submit" value="Enter">
+<body bgcolor="#4863A0">
+
+<div align="center">
+<table>
+	<tr>
+		<td bgcolor="#C7C6C4" width="400" height="30"><font face="cambria"><b>Add a Classes</b></font></td>
+	</tr>
+	<tr>	
+		<td bgcolor="#F9F8F6" width="400" height="150">
+			<form name="form1" method="post" action="save_class.php">
+			<font face="cambria">Enter your class code</font>
+			<input name="Classcode" type="text" id="Classcode">
+			<br><br><input type="submit" name="Submit" value="Add Class"></td>
+	</tr>
+</table>
+</div>
 </body>
 </html>
