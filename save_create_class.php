c2246aee0ec6138ce92766d863d9edec1ccb1550
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-08 13:32:57 +0700

    Update save_create_class.php

--- save_create_class.php
+++ save_create_class.php
@@ -1,8 +1,11 @@
+<html>
+<body>
+<div align="center">
 <?php
 	include("config.php");
 	if(trim($_POST["Classname"]) == "")
 	{
-		echo "Please input Classname!";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Please input Classname!</font>";
 		exit();	
 	}
 	$strSQL = "SELECT * FROM classroom WHERE Classname = '".trim($_POST['Classname'])."' ";
@@ -10,17 +13,20 @@
 	$objResult = mysqli_fetch_array($objQuery);
 	if($objResult)
 	{
-			echo "Class has been create already !";
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Class has been create already !</font>";
 	}
 	else
-	{
-		
+	{		
 		$strSQL = "INSERT INTO classroom (Classname,Aboutclass) VALUES ('".$_POST["Classname"]."', 
 		'".$_POST["Aboutclass"]."')";
 		$objQuery = mysqli_query($objCon,$strSQL);
 		
-		echo "Create Completed!<br>";
-		echo "<br> Go to <a href='display_t.php'>your class room</a>";}
-
+		echo "<font face=\"Cambria\" size=\"5\"color=\"0e5285\">Create Completed!<br></font>";
+		echo "<br><font face=\"Cambria\" color=\"062144\"> Go to <a href='display_t.php'>your class room</a></font>";
+}
 	mysqli_close($objCon);
 ?>
+
+</div>
+</body>
+</html>
