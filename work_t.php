3b053c199b8fb56c7d41e56b40321ce930f6b8d9
nanchonni <38050284+nanchonni@users.noreply.github.com>
2018-04-08 16:46:26 +0700

    Add files via upload

---
+++ work_t.php
@@ -0,0 +1,9 @@
+﻿<html>
+<head>
+<title>Work</title>
+</head>
+<button type = "button" onclick="window.open('popup.php','','width=400,height=400'); return false;"> +ADD </button>
+
+
+</body>
+</html>
