4682847dd53e3dc0a46b7b440c27b4b196dc0624
nanchonni <38050284+nanchonni@users.noreply.github.com>
2018-04-08 16:55:53 +0700

    Add files via upload

---
+++ popup.php
@@ -0,0 +1,12 @@
+﻿<center>
+<html>
+<header>
+<title> Select type </title>
+</header>
+<body>
+<h1>Select type</h1>
+<a href="workquiz.php"> Quiz </a><br>
+<a href="workwork.php"> Work </a>
+</body>
+</html>
+</center>
