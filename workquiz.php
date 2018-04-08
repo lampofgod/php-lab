c39049e4346fbaddf27c73f9dc452ad94bf7ad42
nanchonni <38050284+nanchonni@users.noreply.github.com>
2018-04-08 15:47:45 +0700

    Add files via upload

---
+++ workquiz.php
@@ -0,0 +1,34 @@
+﻿<html>
+<header>
+<title> Work </title>
+</header>
+<body>
+<table cellspacing=10 cellpadding=5 width="400" border="1" style="width: 600px" align="center" bgcolor="white">
+<tbody>
+      <tr>
+        <td width="125"> &nbsp;Google form link</td>
+        <td width="180">
+          <input name="txtGoogle form link" type="text" id="txtGoogle form link" size="55" placeholder="link">
+        </td>
+      </tr>
+	<tr>
+        <td> &nbsp;Start</td>
+        <td><input name="txtStart" type="text" id="txtStart" size="4" placeholder="dd"><input name="txtStart" type="text" id="txtStart" size="4" placeholder="mm"><input name="txtStart" type="text" id="txtStart" size="4" placeholder="yyyy">
+	    <p><input name="txtStart" type="text" id="txtStart" size="4" placeholder="Time"></p>
+        </td>
+      </tr>
+	<tr>
+        <td> &nbsp;End</td>
+        <td><input name="txtEnd" type="text" id="txtEnd" size="4" placeholder="dd"><input name="txtStart" type="text" id="txtStart" size="4" placeholder="mm"><input name="txtStart" type="text" id="txtStart" size="4" placeholder="yyyy">
+	    <p><input name="txtEnd" type="text" id="txtEnd" size="4" placeholder="Time"></p>
+        </td>
+      </tr>
+</tbody>
+</table>
+<br>
+<center>
+<input type="submit" name="Submit" value="Start" align="center" class="button"> 
+<input type="reset" name="Reset" value="Cancel" align="center" class="button">
+</center>
+</body>
+</html>
