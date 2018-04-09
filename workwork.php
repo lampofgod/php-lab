6002a2e91e5107af5f0a7ec47d2f460f0dbfc512
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-09 11:05:27 +0700

    Create workwork.php

---
+++ workwork.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<p style="font-family:cambria"><b>Upload File</b><br><br></p>
+
+<form bgcolor>
+<p style="font-family:cambria">Topic</p>
+<input type="text" name="topic" size="69"><br><br>
+
+<p style="font-family:cambria">Content</p>
+<TEXTAREA name="comment" ROWS="15" COLS="65"></TEXTAREA><br><br>
+<button type="button">Choose File</button><br><br><br>
+<button type="button">Cancel</button>
+<button type="button">Upload</button> 
+</form>
+
+</body>
+</html>
