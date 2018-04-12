ddf94cd2bf8ef595cc37498003d23f9d11828dae
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-12 14:46:11 +0700

    Add files via upload

--- add_ann.php
+++ add_ann.php
@@ -1,8 +1,8 @@
-﻿<html>
+<html>
 <form>
 <h1>Add Your Announcement</h1>
-<p>Topic:</font><input type="text" name="topic"  ></p>
-<p>Content :</font><input type="text" name="context" ></p>
+<p>Topic:<input type="text" name="topic"  ></p>
+<p>Content :</p><textarea rows="10" cols="40"></textarea>
 <p><input type="submit" value="Add"><input type="reset" value="Reset"></p>
 </form>
 </html>  
