3309709d8e9f45130f93f356426fabf6e4a1e4b0
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-13 18:35:38 +0700

    Update add_ann.php

--- add_ann.php
+++ add_ann.php
@@ -1,8 +1,31 @@
 <html>
+<head>
+<style>
+div{ 	background-color:#F9F8F6;
+	font-family:Cambria;
+	width: 500px;
+   	height: 350px;
+	margin-top:100px;
+	background-position: center;
+}
+</style></head>
+
+<body bgcolor="#000044">
+<center>
+<div>
 <form>
 <h1>Add Your Announcement</h1>
-<p>Topic:<input type="text" name="topic"  ></p>
-<p>Content :</p><textarea rows="10" cols="40"></textarea>
-<p><input type="submit" value="Add"><input type="reset" value="Reset"></p>
+<table>
+<tr>
+	<td><p>Topic: <input type="text" name="topic" ></p>
+	<p>Content :</p><textarea rows="10" cols="40"></textarea></td>
+</tr>
+<tr>
+	<td><p align="center"><input type="submit" value="Add"><input type="reset" value="Reset"></p></td></tr>
+</table>
 </form>
+</div>
+</body>
+</center>
+
 </html>  
