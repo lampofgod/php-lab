138fff3c54b597a72795266b9fbc76ac8e7ee9f7
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-13 21:39:23 +0700

    Update student_frame.php

--- student_frame.php
+++ student_frame.php
@@ -7,20 +7,20 @@
 <?php
 
 include("config.php");  
- $strSQL = "SELECT * FROM member ORDER BY UserID asc" or die("Error:" . mysqli_error()); 
+ $strSQL = "SELECT * FROM member WHERE Status = 'STUDENT'" or die("Error:" . mysqli_error()); 
 $objQuery = mysqli_query($objCon,$strSQL);
 	
  
 echo "<table border='1' align='center' width='500'>";
 
-echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ</td><td>นามสกุล</td><td>สถานะ</td><td>อีเมล์</td></tr>";
+echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ</td><td>นามสกุล</td><td>อีเมล์</td><td>สถานะ</td></tr>";
 while($row = mysqli_fetch_array($objQuery)) { 
   echo "<tr>";
   echo "<td>" .$row["UserID"] .  "</td> "; 
   echo "<td>" .$row["txtName"] .  "</td> ";  
   echo "<td>" .$row["txtLastName"] .  "</td> ";
-  echo "<td>" .$row["Status"] .  "</td> ";
   echo "<td>" .$row["txtEmail"] .  "</td> ";
+  echo "<td>" .$row["Status"] .  "</td> ";
 
   echo "</tr>";
 }
