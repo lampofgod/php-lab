f8f84db9cdf80f5186e0fc0ae21e3e1eac11460b
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-15 00:14:17 +0700

    Add files via upload

---
+++ check_s.php
@@ -0,0 +1,8 @@
+<html>
+<form>
+<h1>Make a roll call</h1>
+<p>Check Code:<input type="text" name="code"  ></p>
+<p>Number of your seat :<input type="text" name="code"  ></p>
+<p><input type="submit" value="Add"><input type="reset" value="Reset"></p>
+</form>
+</html>  
