41b621f97cbe5eff2afa90f5a355d5d8fcbc7512
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-15 18:15:46 +0700

    Add files via upload

--- add_ann.php
+++ add_ann.php
@@ -1,8 +1,10 @@
 <html>
+<head><BODY bgcolor="#006699"></BODY>
+</head>
 <form name="form1" method="post" action="save_add_topic.php">
-<h1>Add Your Announcement</h1>
-<p>Topic:<input name="topic" type="text" id="topic"></p>
-<p>Content :</p><textarea rows="10" cols="40" name="content" id="content"></textarea>
+<h1><font face="Cambria" color="white" size="6">Add Your Announcement</font></h1>
+<p><font face="Cambria" color="white" size="4">Topic :</font><input name="topic" type="text" id="topic"></p>
+<p><font face="Cambria" color="white" size="4">Content :</font></p><textarea rows="10" cols="40" name="content" id="content"></textarea>
 <p><input type="submit" value="Add"><input type="reset" value="Reset"></p>
 </form>
-</html> 
+</html>  
