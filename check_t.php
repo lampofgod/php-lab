b4b91888397f2f671c412abbc34140c6bc9b25f2
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-15 17:33:25 +0700

    Update check_t.php

--- check_t.php
+++ check_t.php
@@ -1,8 +1,25 @@
 <html>
+<head>
+<style>
+	div{	background-color:#F9F8F6;
+		font-family:cambria;
+		height:300px;
+		width:400px;
+		margin-top:150px;
+	}
+</style>
+</head>
+<body bgcolor="#000090">
+<center>
 <form>
-<h1>Make a roll call</h1>
-<p>Check Code:<input type="text" name="code"  ></p>
-<p>Number of Attendance :<input type="text" name="code"  ></p>
-<p><input type="submit" value="Add"><input type="reset" value="Reset"></p>
+<div>
+		<h1>Make a roll call</h1><br><br>
+<table><tr><td>	<p>Check Code :&emsp;&emsp;&emsp;&emsp;&emsp;<input type="text" name="code"></p>
+		<p>Number of Attendance : <input type="text" name="code"></p></td></tr></table><br><br><br><br>
+		<p><input type="submit" value="Add">&emsp;<input type="reset" value="Reset"></p>
+</div>
 </form>
-</html>  
+</center>
+</body>
+
+</html>   
