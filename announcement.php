fd2a809fc3b3ab728b765d4d51b4bf1b362922f8
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-15 14:44:20 +0700

    Update announcement.php

--- announcement.php
+++ announcement.php
@@ -6,7 +6,32 @@
 </head>
 
 <body>
-<center>
-<button type = "button" onclick=window.open('add_ann.php','','width=700,height=450'); return false;">+ADD</button></body>
-</center>
+<?php
+    include("config.php");
+	$strSQL = "SELECT * FROM member WHERE UserID = '".$_SESSION['UserID']."' ";
+	$objQuery = mysqli_query($objCon,$strSQL);
+	$objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC);
+	echo $_SESSION["UserID"];
+?>
+<button type = "button" onclick="window.open('add_ann.php','','width=400,height=400'); return false;"> +ADD </button><br /><br /><br /></body>
+
+<?php
+$strSQL = "SELECT * FROM announcement" or die("Error:" . mysqli_error()); 
+$objQuery = mysqli_query($objCon,$strSQL);
+
+
+while($row = mysqli_fetch_array($objQuery)) { 
+echo "<table border='1' align='center' width='500'>";
+  echo "<tr align='left' bgcolor='#CCCCCC'>";
+  echo "<td>" .$row["topic"] .  "</td></tr>"; 
+  echo "<tr><td>" .$row["content"] .  "</td></tr> ";  
+  echo "<tr><td>โพสเมื่อ : " .$row["dateup"] .  "</td></tr>";
+  echo "</tr>";
+  echo "</table><br>";
+}
+
+mysqli_close($objCon);
+	
+?>
+
 </html>
