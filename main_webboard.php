37813a7e928745b31507987c5dcf84f0fa59be45
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-17 17:54:25 +0700

    Add files via upload

--- main_webboard.php
+++ main_webboard.php
@@ -3,25 +3,22 @@
 <title>BLOG</title>
 </head>
 <body>
-<a href="main_newquestion.php">New Topic</a>
+<p align ="center"><button type = "button" onclick="window.open('main_newquestion.php','','width=400,height=400'); return false;"> +ADD NEW TOPIC</button><br /><br /><br /></p>
+
 <?php
 date_default_timezone_set('Asia/Bangkok');
 include("config.php");
 $strSQL = "SELECT * FROM webboard ";
 $objQuery = mysqli_query($objCon,$strSQL) or die("Error Query [".$strSQL."]");
 $Num_Rows = mysqli_num_rows($objQuery);
-
 $Per_Page = 10;   // Per Page
-
 $Page = $_GET["Page"];
 if(!$_GET["Page"])
 {
 	$Page=1;
 }
-
 $Prev_Page = $Page-1;
 $Next_Page = $Page+1;
-
 $Page_Start = (($Per_Page*$Page)-$Per_Page);
 if($Num_Rows<=$Per_Page)
 {
@@ -36,30 +33,29 @@ else
 	$Num_Pages =($Num_Rows/$Per_Page)+1;
 	$Num_Pages = (int)$Num_Pages;
 }
-
 $strSQL .=" order  by QuestionID DESC LIMIT $Page_Start , $Per_Page";
 $objQuery  = mysqli_query($objCon,$strSQL);
 ?>
 <table width="909" border="1">
   <tr>
-    <th width="99"> <div align="center">QuestionID</div></th>
-    <th width="458"> <div align="center">Question</div></th>
-    <th width="90"> <div align="center">Name</div></th>
-    <th width="130"> <div align="center">CreateDate</div></th>
-    <th width="45"> <div align="center">View</div></th>
-    <th width="47"> <div align="center">Reply</div></th>
+    <th width="99" bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">QuestionID</div></font></th>
+    <th width="458"bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">Question</div></font></th>
+    <th width="90"bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">Name</div></font></th>
+    <th width="130"bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">CreateDate</div></font></th>
+    <th width="45"bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">View</div></font></th>
+    <th width="47"bgcolor="#006699"><font face="Cambria" color="white" size="4"> <div align="center">Reply</div></font></th>
   </tr>
 <?php
 while($objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC))
 {
 ?>
   <tr>
-    <td><div align="center"><?php echo $objResult["QuestionID"];?></div></td>
-    <td><a href="main_viewwebboard.php?QuestionID=<?php echo $objResult["QuestionID"];?>"><?php echo $objResult["Question"];?></a></td>
-    <td><?php echo $objResult["Name"];?></td>
-    <td><div align="center"><?php echo $objResult["CreateDate"];?></div></td>
-    <td align="right"><?php echo $objResult["View"];?></td>
-    <td align="right"><?php echo $objResult["Reply"];?></td>
+    <td bgcolor="#99CCCC"><div align="center"><?php echo $objResult["QuestionID"];?></div></td>
+    <td bgcolor="#99CCCC"><a href="main_viewwebboard.php?QuestionID=<?php echo $objResult["QuestionID"];?>"><?php echo $objResult["Question"];?></a></td>
+    <td bgcolor="#99CCCC"><?php echo $objResult["Name"];?></td>
+    <td bgcolor="#99CCCC"><div align="center"><?php echo $objResult["CreateDate"];?></div></td>
+    <td align="right" bgcolor="#99CCCC"><?php echo $objResult["View"];?></td>
+    <td align="right" bgcolor="#99CCCC"><?php echo $objResult["Reply"];?></td>
   </tr>
 <?php
 }
@@ -73,7 +69,6 @@ if($Prev_Page)
 {
 	echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$Prev_Page'><< Back</a> ";
 }
-
 for($i=1; $i<=$Num_Pages; $i++){
 	if($i != $Page)
 	{
