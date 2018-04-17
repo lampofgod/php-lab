e5fe2a75efea1e79dc2ec665cab3650f125ef262
0129RAINBOW <38044854+0129RAINBOW@users.noreply.github.com>
2018-04-17 14:26:06 +0700

    Update file_2.php

--- file_2.php
+++ file_2.php
@@ -1,25 +1,31 @@
 <html>
 <head>
 <title>ThaiCreate.Com Tutorial</title>
+<style>
+	table{	font-family:cambria;
+		background-color:#F9F8F6;
+	}
+</style>
 </head>
-<body>
+<body bgcolor="#00004A">
+<center>
 <?php
 	include('connection.php'); 
 $query = "SELECT * FROM uploadfile ORDER BY fileID asc" or die("Error:" . mysqli_error()); 
 $result = mysqli_query($con, $query);
 ?>
-<table width="200" border="1">
+<table width="600" border="1">
 <tr>
-<th width="50"> <div align="center">Files ID </div></th>
-<th width="150"> <div align="center">Files Name </div></th>
+<th width="50" bgcolor="#E5E4E2"> <div align="center">Files ID</div></th>
+<th width="550"> <div align="center">Files Name</div></th>
 </tr>
 <?php
 	while($row = mysqli_fetch_array($result))
 	{
 ?>
 <tr>
-<td><div align="center"><?php echo $row["fileID"];?></div></td>
-<td><center><a href="fileupload/<?php echo $row["fileupload"];?>"><?php echo $row["fileupload"];?></a></center></td>
+<td bgcolor="#E5E4E2"><div align="center"><?php echo $row["fileID"];?></div></td>
+<td><a href="fileupload/<?php echo $row["fileupload"];?>"><?php echo $row["fileupload"];?></a></td>
 </tr>
 <?php
 	}
@@ -29,4 +35,5 @@ $result = mysqli_query($con, $query);
 mysqli_close($con);
 ?>
 </body>
+</center>
 </html>
