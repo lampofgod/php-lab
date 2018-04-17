c50acfe7ae44208800684de4f9580838e127d8a6
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-17 17:47:55 +0700

    Update main_viewwebboard.php

--- main_viewwebboard.php
+++ main_viewwebboard.php
@@ -1,10 +1,6 @@
 <?php
-define('servername','webservhost');
-define('username','lampofgod_lamp');
-define('password','025179750');
-define('dbname','lampofgod_lamp');
-$objConnect = mysqli_connect(servername,username,password,dbname);
-
+date_default_timezone_set('Asia/Bangkok');
+include("config.php");
 if($_GET["Action"] == "Save")
 {
 	//*** Insert Reply ***//
@@ -12,39 +8,39 @@ if($_GET["Action"] == "Save")
 	$strSQL .="(QuestionID,CreateDate,Details,Name) ";
 	$strSQL .="VALUES ";
 	$strSQL .="('".$_GET["QuestionID"]."','".date("Y-m-d H:i:s")."','".$_POST["txtDetails"]."','".$_POST["txtName"]."') ";
-	$objQuery = mysqli_query($strSQL);
+	$objQuery = mysqli_query($objCon,$strSQL);
 	
 	//*** Update Reply ***//
 	$strSQL = "UPDATE webboard ";
 	$strSQL .="SET Reply = Reply + 1 WHERE QuestionID = '".$_GET["QuestionID"]."' ";
-	$objQuery = mysqli_query($strSQL);	
+	$objQuery = mysqli_query($objCon,$strSQL);	
 }
 ?>
 <html>
 <head>
-<title>ThaiCreate.Com</title>
+<title>BLOG</title>
 </head>
 <body>
 <?php
 //*** Select Question ***//
 $strSQL = "SELECT * FROM webboard  WHERE QuestionID = '".$_GET["QuestionID"]."' ";
-$objQuery = mysqli_query($strSQL) or die ("Error Query [".$strSQL."]");
+$objQuery = mysqli_query($objCon,$strSQL) or die ("Error Query [".$strSQL."]");
 $objResult = mysqli_fetch_array($objQuery);
-
 //*** Update View ***//
 $strSQL = "UPDATE webboard ";
 $strSQL .="SET View = View + 1 WHERE QuestionID = '".$_GET["QuestionID"]."' ";
-$objQuery = mysqli_query($strSQL);	
+$objQuery = mysqli_query($objCon,$strSQL);	
 ?>
 <table width="738" border="1" cellpadding="1" cellspacing="1">
   <tr>
     <td colspan="2"><center><h1><?php echo $objResult["Question"];?></h1></center></td>
   </tr>
   <tr>
-    <td height="53" colspan="2"><?php echonl2br($objResult["Details"]);?></td>
+    <td height="53" colspan="2"><?php echo nl2br($objResult["Details"]);?></td>
   </tr>
   <tr>
     <td width="397">Name : <?php echo $objResult["Name"];?> Create Date : <?php echo $objResult["CreateDate"];?></td>
+    <td width="253">View : <?php echo $objResult["View"];?> Reply : <?php echo $objResult["Reply"];?></td>
   </tr>
 </table>
 <br>
@@ -52,14 +48,14 @@ $objQuery = mysqli_query($strSQL);
 <?php
 $intRows = 0;
 $strSQL2 = "SELECT * FROM reply  WHERE QuestionID = '".$_GET["QuestionID"]."' ";
-$objQuery2 = mysqli_query($strSQL2) or die ("Error Query [".$strSQL."]");
+$objQuery2 = mysqli_query($objCon,$strSQL2) or die ("Error Query [".$strSQL."]");
 while($objResult2 = mysqli_fetch_array($objQuery2))
 {
 	$intRows++;
 ?> No : <?php echo $intRows;?>
 <table width="738" border="1" cellpadding="1" cellspacing="1">
   <tr>
-    <td height="53" colspan="2"><?php echonl2br($objResult2["Details"]);?></td>
+    <td height="53" colspan="2"><?php echo nl2br($objResult2["Details"]);?></td>
   </tr>
   <tr>
     <td width="397">Name :
@@ -72,9 +68,9 @@ while($objResult2 = mysqli_fetch_array($objQuery2))
 }
 ?>
 <br>
-<a href="Webboard.php">Back to Webboard</a> <br>
+<a href="main_webboard.php">Back to Webboard</a> <br>
 <br>
-<form action="ViewWebboard.php?QuestionID=<?php echo $_GET["QuestionID"];?>&Action=Save" method="post" name="frmMain" id="frmMain">
+<form action="main_viewwebboard.php?QuestionID=<?php echo $_GET["QuestionID"];?>&Action=Save" method="post" name="frmMain" id="frmMain">
   <table width="738" border="1" cellpadding="1" cellspacing="1">
     <tr>
       <td width="78">Details</td>
@@ -91,5 +87,5 @@ while($objResult2 = mysqli_fetch_array($objQuery2))
 </body>
 </html>
 <?php
-mysqli_close($objConnect);
+mysqli_close($objCon);
 ?>
