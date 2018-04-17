ddd208dcf61119f920da965b1b66bf7553463fce
59010372 <38009029+59010372@users.noreply.github.com>
2018-04-17 13:17:01 +0700

    Add files via upload

--- add_work.php
+++ add_work.php
@@ -1,37 +1,38 @@
-<html>
+﻿<html>
 <body>
+<BODY bgcolor="#E6E6FA">
 <form action="save_add_work.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
-<p style="font-family:cambria">Topic</p>
-<input type="text" name="topic" id="topic" size="69"><br><br>
+<p ><font face="Cambria" color="black" size="6">Topic</font></p>
+<input name="topic" type="text" id="topic"><br><br>
 
-<p style="font-family:cambria">Content</p>
+<p><font face="Cambria" color="black" size="6">Content</font></p>
 <TEXTAREA name="content" id="content" ROWS="15" COLS="65"></TEXTAREA><br><br>
 
   <p>&nbsp;</p>
   <table width="700" border="0" align="center" cellpadding="0" cellspacing="0">
     <tr>
-      <td height="40" colspan="2" align="center" bgcolor="#D6D6D6">Form Upload&nbsp;File</td>
+      <td height="40" colspan="2" align="center" bgcolor="#006699"><font face="Cambria" color="white" size="6">Form Upload&nbsp;File</font></td>
     </tr>
     <tr>
-      <td width="126" bgcolor="#EDEDED">&nbsp;</td>
-      <td width="574" bgcolor="#EDEDED">&nbsp;</td>
+      <td width="126" bgcolor="#99CCCC">&nbsp;</td>
+      <td width="574" bgcolor="#99CCCC">&nbsp;</td>
     </tr>
     <tr>
-      <td align="center" bgcolor="#EDEDED">File Browser</td>
-      <td bgcolor="#EDEDED"><label>
+      <td align="center" bgcolor="#99CCCC">File Browser</td>
+      <td bgcolor="#99CCCC"><label>
         <input type="file" name="fileupload" id="fileupload"  required="required"/>
       </label></td>
     </tr>
     <tr>
-      <td bgcolor="#EDEDED">&nbsp;</td>
-      <td bgcolor="#EDEDED">&nbsp;</td>
+      <td bgcolor="#99CCCC">&nbsp;</td>
+      <td bgcolor="#99CCCC">&nbsp;</td>
     </tr>
   </table>
   <p>&nbsp;</p>
 
 <br/>
 
-<input type="submit" value="Add">
+<p align="center"><input type="submit" value="Add"></p>
 </form>
 
 </body>
