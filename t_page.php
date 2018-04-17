4265b9402e2d31863f0a84c57ce76e941aa0d957
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-17 13:05:25 +0700

    Update t_page.php

--- t_page.php
+++ t_page.php
@@ -1,44 +1,61 @@
 <?php
-	session_start();
+ session_start();
         include("config.php");
-	if($_SESSION['UserID'] == "")
-	{
-		echo "Please Login!";
-		exit();
-	}
+ if($_SESSION['UserID'] == "")
+ {
+  echo "Please Login!";
+  exit();
+ }
 
-	if($_SESSION['Status'] != "TEACHER")
-	{
-		echo "This page for Teacher only!";
-		exit();
-	}	
-	
+ if($_SESSION['Status'] != "TEACHER")
+ {
+  echo "This page for Teacher only!";
+  exit();
+ } 
+ 
 
-	$strSQL = "SELECT * FROM member WHERE UserID = '".$_SESSION['UserID']."' ";
-	$objQuery = mysqli_query($objCon,$strSQL);
-	$objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC);
+ $strSQL = "SELECT * FROM member WHERE UserID = '".$_SESSION['UserID']."' ";
+ $objQuery = mysqli_query($objCon,$strSQL);
+ $objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC);
 ?>
 <html>
 
 <body>
-  Welcome to Teacher Page! <br>
+  Welcome <?php echo $objResult["txtName"];?>&nbsp;<?php echo $objResult["txtLastName"];?> <br>
   <table border="1" style="width: 300px">
     <tbody>
       <tr>
-        <td width="87"> &nbsp;Username</td>
-        <td width="197"><?php echo $objResult["txtUsername"];?>
+        <td width="87"> &nbsp;UserID</td>
+        <td width="197"><?php echo $objResult["UserID"];?>
         </td>
       </tr>
       <tr>
-        <td> &nbsp;Name</td>
-        <td><?php echo $objResult["txtName"];?></td>
+        <td> &nbsp;Status</td>
+        <td><?php echo $objResult["Status"];?></td>
       </tr>
     </tbody>
   </table>
   <br>
-  <a href="edit_profile.php">Edit</a>
+  <a href="edit_profile.php">Edit profile</a>
   <br>
   <a href="create_class.php">Create classroom</a>
+  <br>
+  Teaching : 
+  <br>
+  <?php
+  $strSQL = "SELECT * FROM classroom ORDER BY Classcode asc" or die("Error:" . mysqli_error()); 
+$objQuery = mysqli_query($objCon,$strSQL);
+echo "<table border='1'>";
+echo "<tr align='center' bgcolor='#CCCCCC'><td>Classcode</td><td>Classname</td><td>Aboutclass</td></tr>";
+while($row = mysqli_fetch_array($objQuery)) { 
+  echo "<tr>";
+ echo '<td>'.$row["Classcode"] .  "</a></td> "; 
+ echo '<td><a href="display_t.php" target="_blank">' .$row["Classname"] .  "</a></td> "; 
+ echo '<td>'.$row["Aboutclass"] .  "</a></td> "; 
+  echo "</tr>";
+}
+echo "</table>";
+  ?>
   <br>
   <a href="add_class.php">Add classroom</a>
   <br>
@@ -46,5 +63,5 @@
 </body>
 </html>
 <?php
-	mysqli_close($objCon);
+ mysqli_close($objCon);
 ?>
