3bb94a8c82f0bc28679ca6e82ab7c61efebd4d63
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-17 23:19:54 +0700

    Update check_s.php

--- check_s.php
+++ check_s.php
@@ -10,15 +10,12 @@
 </style>
 </head>
 <body bgcolor="#151B54">
-<center>
-<form>
-<div>		<h1>Make a roll call</h1><br><br>
-<table><tr><td>	Check Codes :&emsp;&emsp;&emsp;&ensp;<input type="text" name="code">
-		<p>Number of your seat : <input type="text" name="code"></p></td></tr></table><br><br><br><br>
-	<input type="submit" value="Add">&emsp;<input type="reset" value="Reset">
-</div>
+<form name="form1" method="post" action="save_code.php">
+<center><div><br>
+<h1>Make a roll call</h1>
+Check Codes  <br /><input name="code" type="text" id="code" size="6"><br />
+Number of your seat <br /><input name="code_num" type="text" id="code_num" size="2"><br /><br>
+<input type="submit" name="Submit" value="Save"></form>
 
-</form>
-</center>
 </body>
-</html>  
+</html> 
