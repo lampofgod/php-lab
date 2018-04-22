b392f16298aa6a03161bc3a8e5b033b85be24ba7
nanchonni <38050284+nanchonni@users.noreply.github.com>
2018-04-22 17:17:35 +0700

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,25 +1,35 @@
-<center>
-<style>
-h1{color: red;}
-body{background-color: #bde3ff;}
-</style>
-<body>
+<? ob_start() ?>
 <?php
 	session_start();
         include("config.php");
 	$strSQL = "SELECT * FROM member WHERE txtUsername = '".mysqli_real_escape_string($objCon,$_POST['txtUsername'])."' 
 	and txtPassword = '".mysqli_real_escape_string($objCon,$_POST['txtPassword'])."'";
+ 
+ $captcha;
+ if(isset($_POST['g-recaptcha-response'])){
+ $captcha=$_POST['g-recaptcha-response'];
+ }
+ if(!$captcha){
+ echo '<h2>Please confirm your identity.</h2>';
+ exit;
+ }
+
 	$objQuery = mysqli_query($objCon,$strSQL);
 	$objResult = mysqli_fetch_array($objQuery);
 	if(!$objResult)
 	{
-			echo "<h1>Username and Password Incorrect!</h1>\n\n\n";
-			echo '<a href="home_test.php"><img src="back1.png"/></a>';
+			echo "Username and Password Incorrect!\n\n\n";
+			echo "<a href='home_test.php'><h3>back</h3></a>"; 
+            exit();      
 	}
+	
+
 	else
 	{
 			$_SESSION["UserID"] = $objResult["UserID"];
 			$_SESSION["Status"] = $objResult["Status"];
+			$_SESSION["txtName"] = $objResult["txtName"];
+			$_SESSION["txtLastName"] = $objResult["txtLastName"];
 			session_write_close();
 			
 			if($objResult["Status"] == "TEACHER")
@@ -33,5 +43,4 @@ body{background-color: #bde3ff;}
 	}
 	mysqli_close($objCon);
 ?>
-</body>
-</center>
+<? ob_end_flush() ?>
