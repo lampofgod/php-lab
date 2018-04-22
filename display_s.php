6c3b7da88b77215039ce43cf8ad667928fe696f0
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-22 11:25:47 +0700

    Update display_s.php

--- display_s.php
+++ display_s.php
@@ -1,10 +1,9 @@
 <html>
 <head><title> Classroom</title></head>
 
-<frameset rows="100,50,*" cols="*" frameborder="no" border="0" framespacing="0">
-<frame src="top1.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
-<frame src="top2.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
-<frame src="announcement.php" name="mainFrame" id="mainFrame" 	title="mainFrame" >
+<frameset rows="220,*" cols="*" frameborder="no" border="0" framespacing="0">
+<frame src="top2_s.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
+<frame src="announcement_s.php" name="mainFrame" id="mainFrame" 	title="mainFrame" >
 		
 		
 </frameset><noframes></noframes>
