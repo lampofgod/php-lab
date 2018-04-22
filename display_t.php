89a157911d22bdc3af11605a93c85a86f60be923
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-22 11:25:39 +0700

    Update display_t.php

--- display_t.php
+++ display_t.php
@@ -1,7 +1,7 @@
 <html>
 <head><title> Classroom</title></head>
 
-<frameset rows="170,*" cols="*" frameborder="no" border="0" framespacing="0">
+<frameset rows="220,*" cols="*" frameborder="no" border="0" framespacing="0">
 <frame src="top2.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" >
 <frame src="announcement.php" name="mainFrame" id="mainFrame" 	title="mainFrame" >
 		
