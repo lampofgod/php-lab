d0500203607cd6698fb90ab59ddd1cf1f2bc2894
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-24 19:15:51 +0700

    Update s_page.php

--- s_page.php
+++ s_page.php
@@ -1,5 +1,7 @@
 <?php
 	session_start();
+	//$Classcode=$_SESSION['$Classcode'];
+	
 include("config.php");
 	if($_SESSION['UserID'] == "")
 	{
@@ -22,24 +24,45 @@ include("config.php");
 <html>
 
 <body>
-  Welcome <?php echo $objResult["txtName"];?></td> <br>
+  Welcome <?php echo $objResult["txtName"];?>&nbsp;<?php echo $objResult["txtLastName"];?></td> <br>
   <table border="1" style="width: 300px">
     <tbody>
       <tr>
-        <td width="87"> &nbsp;Username</td>
-        <td width="197"><?php echo $objResult["txtUsername"];?>
+        <td width="87"> &nbsp;UserID</td>
+        <td width="197"><?php echo $objResult["UserID"];?>
         </td>
       </tr>
       <tr>
-        <td> &nbsp;Name</td>
-        <td><?php echo $objResult["txtName"];?></td>
+        <td> &nbsp;Status</td>
+        <td><?php echo $objResult["Status"];?></td>
       </tr>
     </tbody>
   </table>
   <br>
-  <a href="edit_profile.php">Edit</a>
+  <a href="edit_profile.php">Edit profile</a>
   <br>
-  <a href="add_class.php">Add classroom</a>
+  <a href="add_class.php">Add classroom</a><br>
+ Subject  : 
+  <br>
+  <?php
+  $strSQL = "SELECT * FROM classroom,join_class WHERE classroom.Classcode=join_class.Classcode && join_class.UserID = '".$objResult["UserID"]."' ORDER BY join_class.Classcode asc" or die("Error:" . mysqli_error()); 
+$objQuery = mysqli_query($objCon,$strSQL);
+echo "<table border='1'>";
+echo "<tr align='center' bgcolor='#CCCCCC'><td>Classcode</td><td>Classname</td><td>Aboutclass</td></tr>";
+while($row = mysqli_fetch_array($objQuery)) {
+	//if($row["UserID"] == $objResult["UserID"]){
+  echo "<tr>";
+ echo "<td>".$row["Classcode"].  "</td> "; 
+ //echo '<td><a href="display_t.php" target="_blank">' .$row["Classname"] .  "</a></td> ";
+  ?>
+<td><a href="https://webserv.kmitl.ac.th/lampofgod/display_s.php?Classcode=<?php echo $row["Classcode"];?>"><?php echo $row["Classname"];?></a></td>
+
+<?php
+echo '<td>'.$row["Aboutclass"] .  "</a></td> "; 
+  echo "</tr>";
+	}//}
+echo "</table>";
+  ?>
   <br>
   <a href="logout.php">Logout</a>
 </body>
