f8c9c5418bb151263dc820deccb4d7ea34f2b697
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-29 10:41:18 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,34 +1,25 @@
-<?php session_start(); 
- include("database.php");
+<?php
+session_start(); 
+$UserID=$_SESSION['UserID'];
+$Classname=$_SESSION['Classname'];
+$txtName=$_SESSION['txtName'];
+$txtLastName=$_SESSION['txtLastName'];
+include 'connection.php';
 
-      //Check to see if score is set_error_handler
-	if (!isset($_SESSION['score'])){
-	   $_SESSION['score'] = 0;
+if(trim($_POST["message"]) == "")
+	{
+		echo "Please input message !";
+		exit();	
 	}
-//Check if form was submitted
-if($_POST){
-	$number = $_POST['number'];
-	$selected_choice = $_POST['choice'];
-	$next=$number+1;
-	$total=4;
-	//Get total number of questions
-	$query="SELECT * FROM `questions`";
-	$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
-	$total=$results->num_rows;
-	//Get correct choice
-	$q = "select * from `choices` where question_number = $number and is_correct=1";
-	$result = $mysqli->query($q) or die($mysqli->error.__LINE__);
-	$row = $result->fetch_assoc();
-	$correct_choice=$row['id'];
-	//compare answer with result
-	if($correct_choice == $selected_choice){
-		$_SESSION['score']++;
-	}
-	if($number == $total){
-		header("location: final.php");
-		exit();
-	} else {
-	        header("location:question.php?n=".$next."&score=".$_SESSION['score']);
-	}
+else{
+	date_default_timezone_set('Asia/Bangkok');
+	$time = $date = date("Y-m-d H:i:s");
+$strSQL = "INSERT INTO shouts (user,message,time,Classname) VALUES ('".$txtName."', 
+		'".$_POST["message"]."','".$time."','".$Classname."')";
+		$objQuery = mysqli_query($con,$strSQL);
+				header("location: chatroom.php");
+				exit();
 }
+		
+
 ?>
