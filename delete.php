d47ed6299e20e9345caacde693de82ccf323828b
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-29 10:49:11 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,11 @@
-<html>
-<head>
-<style>
-		body{background-image :url(1.jpg);}
-</style>
-<title>delete</title>
-</head>
-<body>
-<?php
-include("config1.php");
+<?php session_start();
+include("config.php");
+$dateInput=$_SESSION['dateInput'];
 $sql = "DELETE FROM student_check WHERE Hello='".$_GET[Hello]."'";
 if(mysqli_query($objCon,$sql)){
-	echo "ลบข้อมูลเรียบร้อย";
+	echo "<meta http-equiv ='refresh'content='0;URL=teacherclassresult.php'>";
 }else{
 	echo mysqli_error();
 }
 mysqli_close($objCon);
-?><br><br>
-<input type="button" align="left" value="Back" button onclick="location.href='teacherdate.php'"></body>
-</body>
-</html>
+?>
