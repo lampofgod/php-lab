d2a64e4dd99d872b818a3e2fc59cee0525fe0cdf
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-29 10:38:13 +0700

    Update save_add_topic.php

--- save_add_topic.php
+++ save_add_topic.php
@@ -1,7 +1,12 @@
 <?php
+session_start(); 
+$UserID=$_SESSION['UserID'];
+$Classname=$_SESSION['Classname'];
 include("config.php");
-date_default_timezone_set("Asia/Bangkok");
-$date = date("d-m-Y"); 
+/*date_default_timezone_set("Asia/Bangkok");
+$date = date("d-m-Y"); */
+date_default_timezone_set('Asia/Bangkok');
+	$time = $date = date("Y-m-d H:i:s");
 if(trim($_POST["topic"]) == "")
 	{
 		echo "Please input topic !";
@@ -13,7 +18,7 @@ if(trim($_POST["content"]) == "")
 		exit();	
 	}
 else{
-$strSQL = "INSERT INTO announcement (topic,content) VALUES ('".$_POST["topic"]."', '".$_POST["content"]."')";
+$strSQL = "INSERT INTO announcement (topic,content,Classname,dateup) VALUES ('".$_POST["topic"]."', '".$_POST["content"]."','".$Classname."','".$time."')";
 		$objQuery = mysqli_query($objCon,$strSQL);
 		echo "Add announcement Completed !<br>";		
 		echo "<script type='text/javascript'>";
