052f93792ddb4b9c98c5a93132bd0f0e60363ccb
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-29 10:36:31 +0700

    Update top2_s.php

--- top2_s.php
+++ top2_s.php
@@ -15,7 +15,7 @@ session_start();
 <style>
 .button {
     background-color: #0e5285;
-    width: 12%;
+    width: 10%;
     border: none;
     color: white;
     padding: 15px 40px;
@@ -34,14 +34,18 @@ session_start();
 <h3 align="right"><?php echo $objResult["Status"];?>
 <h3 align="right"><?php echo $objResult["txtName"];?>&nbsp;<?php echo $objResult["txtLastName"];?></h3>
 
-<h5 align="right">Classcode : <?php echo $_SESSION["Classcode"];?></h5>
-<a href="edit_profile.php" target="new"><p align="right">Edit</a>
+<h5 align="right">Classcode : <?php echo $_SESSION["Classcode"];?> Classname : <?php echo $_SESSION["Classname"];?></h5>
+<a href="s_page.php" target="new"><p align="right">Home</a>
+<a href="edit_profile.php" target="new">Edit</a>
   <a href="logout.php" target="new">Logout</p></a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="announcement_s.php"target="mainFrame"> <input  value="Announcement" align="center"    class="button" ></a>
+<a href="calendar_s.php"target="mainFrame">			<input  value="Calendar"		 align="center"    class="button" ></a>
 <a href="work_s.php"target="mainFrame">			<input  value="Work"		 align="center"    class="button" ></a>
 <a href="student_frame_s.php"target="mainFrame"><input  value="Student" 	 align="center"    class="button" ></a>
+<a href="studentnew.php"target="mainFrame">			<input  value="Attendance" 		 align="center"    class="button" ></a>
 <a href="file_s.php"target="mainFrame">			<input  value="File"		 align="center"    class="button" ></a>
+<a href="chatroom.php"target="mainFrame">			<input  value="Chatroom" 		 align="center"    class="button" ></a>
 <a href="main_webboard.php"target="mainFrame">			<input  value="Blog" 		 align="center"    class="button" ></a>
 </body>
 </html>
