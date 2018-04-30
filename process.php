8f81e8bcb3b5a030df14441cf980802a447c5b00
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-30 11:55:17 +0700

    Update process.php

--- process.php
+++ process.php
@@ -4,18 +4,22 @@ $UserID=$_SESSION['UserID'];
 $Classname=$_SESSION['Classname'];
 $txtName=$_SESSION['txtName'];
 $txtLastName=$_SESSION['txtLastName'];
+$Classcode=$_SESSION['Classcode'];
 include 'connection.php';
 
 if(trim($_POST["message"]) == "")
 	{
 		echo "Please input message !";
+		?>
+        <a href="chatroom.php">back to chatbox</a>
+        <?php
 		exit();	
 	}
 else{
 	date_default_timezone_set('Asia/Bangkok');
 	$time = $date = date("Y-m-d H:i:s");
-$strSQL = "INSERT INTO shouts (user,message,time,Classname) VALUES ('".$txtName."', 
-		'".$_POST["message"]."','".$time."','".$Classname."')";
+$strSQL = "INSERT INTO shouts (user,message,time,Classcode) VALUES ('".$txtName."', 
+		'".$_POST["message"]."','".$time."','".$Classcode."')";
 		$objQuery = mysqli_query($con,$strSQL);
 				header("location: chatroom.php");
 				exit();
