8cdd44537f03289b617203f5d8e6a71fcadccf22
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-04-30 11:53:46 +0700

    Update work.php

--- work.php
+++ work.php
@@ -1,3 +1,8 @@
+<?php  session_start(); 
+$UserID=$_SESSION['UserID'];
+$Classname=$_SESSION['Classname'];
+$Classcode=$_SESSION['Classcode'];
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -11,16 +16,16 @@
 
 <?php
 include("config.php");
-$strSQL = "SELECT * FROM work" or die("Error:" . mysqli_error()); 
+$strSQL = "SELECT * FROM work WHERE Classcode = '".$Classcode."' ORDER BY workID DESC" or die("Error:" . mysqli_error()); 
 $objQuery = mysqli_query($objCon,$strSQL);
 while($row = mysqli_fetch_array($objQuery)) { 
 echo "<table border='1' align='center' width='500'>";
   echo "<tr align='left' bgcolor='#006699'>";
   echo "<td><font face='Cambria' color='white' size='4'>" .$row["topic"] .  "</font></td></tr>"; 
   echo "<tr bgcolor='#E6E6FA'><td>" .$row["content"] .  "</td></tr> ";  
-  echo "<tr bgcolor='#E6E6FA'><td>â¾ÊàÁ×èÍ : " .$row["dateup"] .  "</td></tr>";
+  echo "<tr bgcolor='#E6E6FA'><td>à¹‚à¸žà¸ªà¹€à¸¡à¸·à¹ˆà¸­ : " .$row["dateup"] .  "</td></tr>";
   ?>
-  <td bgcolor='#E6E6FA'><center><a href="myfile/<?php echo $row["fileupload"];?>"><?php echo $row["fileupload"];?></a></center></td>
+  <td bgcolor='#E6E6FA'><center><a href="myfile/<?php echo $row["fileupload"];?>" target="_blank"><?php echo $row["fileupload"];?></a></center></td>
   <?php
   echo "</tr>";
   echo "</table><br>";
