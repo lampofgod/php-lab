395e469d95b4bd292dd37d2b3402866e4fac9017
lampofgod <38007560+lampofgod@users.noreply.github.com>
2018-05-01 12:43:35 +0700

    Update teacherdate.php

--- teacherdate.php
+++ teacherdate.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+ include("config.php");
+$Classname=$_SESSION['Classname'];
+?>
 <!doctype html>
 <html>
 <head>
@@ -35,5 +40,29 @@ $(function(){
 <br><br>
 <input type="submit" value="Submit">
 </form>
+
+<?php
+
+
+$objQuery = "SELECT COUNT(Seat), student_check.UserID, txtName, txtLastName, Classname FROM student_check,member WHERE student_check.UserID=member.UserID && Classname='".$Classname."' GROUP BY student_check.UserID ORDER BY UserID ASC" or die("Error:" . mysqli_error()); 
+$objResult = mysqli_query($objCon, $objQuery); 
+echo "<p> <font size='7pt'> ตารางแสดงจำนวนครั้งที่เข้าเรียน</font></p>";
+echo "<table border='1' width='500'>";
+echo "<tr align='center' bgcolor='#CCCCCC'><td>No</td><td>UserID</td><td>name</td><td>LastName</td><td>จำนวนครั้ง</td></tr>";
+$item = 1;
+while($row = mysqli_fetch_array($objResult)) { 
+  echo "<tr>";
+  echo "<td>" .$item.  "</td> "; 
+  echo "<td>" .$row["UserID"] .  "</td> "; 
+  echo "<td>" .$row["txtName"] .  "</td> ";  
+  echo "<td>" .$row["txtLastName"] .  "</td> ";  
+  echo "<td>" .$row["COUNT(Seat)"].  "</td> ";
+  echo "</tr>";
+ $item++;
+}
+echo "</table>";
+mysqli_close($objCon);
+?>
+
 </body>
 </html>
